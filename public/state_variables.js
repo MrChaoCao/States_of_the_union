751c4f09b24616851124b9ca7beb911c67680c85
chao <mailbox.chao.cao@gmail.com>
2018-06-05 10:18:51 -0700

    initial heroku push

---
+++ public/state_variables.js
@@ -0,0 +1,61 @@
+export const state_map_array =
+[
+  NaN, 0.00, 0.00,  NaN, 0.00, 0.00, 0.00,  NaN, 0.00, 0.00,
+  0.00,  NaN, 0.00, 0.00,  NaN, 0.00, 0.00, 0.00, 0.00, 0.00,
+  0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00,
+  0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00,
+  0.00, 0.00, 0.00,  NaN, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00,
+  0.00, 0.00,  NaN, 0.00, 0.00, 0.00, 0.00
+];
+
+export const states = new Object()
+  states.AL = 1;
+  states.AK = 2;
+  states.AZ = 4;
+  states.AR = 5;
+  states.CA = 6;
+  states.CO = 8;
+  states.CT = 9;
+  states.DE = 10;
+  states.FL = 12;
+  states.GA = 13;
+  states.HI = 15;
+  states.ID = 16;
+  states.IL = 17;
+  states.IN = 18;
+  states.IA = 19;
+  states.KS = 20;
+  states.KY = 21;
+  states.LA = 22;
+  states.ME = 23;
+  states.MD = 24;
+  states.MA = 25;
+  states.MI = 26;
+  states.MN = 27;
+  states.MS = 28;
+  states.MO = 29;
+  states.MT = 30;
+  states.NE = 31;
+  states.NV = 32;
+  states.NH = 33;
+  states.NJ = 34;
+  states.NM = 35;
+  states.NY = 36;
+  states.NC = 37;
+  states.ND = 38;
+  states.OH = 39;
+  states.OK = 40;
+  states.OR = 41;
+  states.PA = 42;
+  states.RI = 44;
+  states.SC = 45;
+  states.SD = 46;
+  states.TN = 47;
+  states.TX = 48;
+  states.UT = 49;
+  states.VT = 50;
+  states.VA = 51;
+  states.WA = 53;
+  states.WV = 54;
+  states.WI = 55;
+  states.WY = 56;
---
+++ secrets.js
@@ -0,0 +1,1 @@
+export const consumer_key = 'q6kzEP9qMkVrM66ujiR1UK5ga', 
