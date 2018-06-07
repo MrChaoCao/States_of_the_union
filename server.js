4efc5f91051bc165c6a9cdc79ed7078a27864411
chao <mailbox.chao.cao@gmail.com>
2018-06-07 10:49:07 -0700

    Include tweet-information and tweet_url in data output

--- server.js
+++ server.js
@@ -54,6 +54,11 @@ io.sockets.on('connection', function (socket) {
               "text": data.text,
               "hashtags": data.entities.hashtags,
               "profile_pic": data.user.profile_image_url_https,
+              "created_at": data.created_at,
+              "tweet_url" `https://twitter.com/${data.user}/status/${data.id}`,
+              "retweet_count": data.retweet_count,
+              "favorite_count": data.favorite_count,
+              "reply_count": data.reply_count,
             }
 
             socket.broadcast.emit("twitter-states", tweetInfo);
